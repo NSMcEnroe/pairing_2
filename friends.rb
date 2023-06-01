56a442565ce19b17f27bff55c756b5837ff18c20
Nicholas McEnroe <nsmcenro@gmail.com>
2023-06-01 17:18:16 -0400

    Add places to visit

--- friends.rb
+++ friends.rb
@@ -2,3 +2,9 @@ arrays = ["Tyler","Noelle","Kaitlyn","Eliza","Nick"]
 puts arrays[2]
 puts arrays[4]
 puts arrays [0]
+
+places_to_visit = ["Paris", "Prague", "Tokyo", "Dublin", "Bend"]
+
+places_to_visit.pop
+
+puts places_to_visit
